848b5f96d43b6330c204d82ca2762fbca5aff65a
Alvin <alvinramoutar@gmail.com>
2016-09-18 16:22:10 -0400

    Added a hyperlink ability for first verse of lyrical text.

--- snRTS-planning/src/game/Menu.java
+++ snRTS-planning/src/game/Menu.java
@@ -1,5 +1,10 @@
 package game;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
@@ -21,8 +26,10 @@ public class Menu extends BasicGameState {
 		Image background = new Image("res/img.jpg");
 		g.drawImage(background, 0, 0);
 		
+		//ONLY this text will open the webpage. Refer to mouse tracking code below.
 		g.drawString("That place between asleep and awake,", 50, 50);
 		g.drawString("where you can still remember dreams...", 70, 70);
+		
 		g.drawString("...that's where I'll always love you,", 200, 420);
 		g.drawString("and thats where I'll be waiting.", 220, 440);
 		
@@ -43,6 +50,24 @@ public class Menu extends BasicGameState {
 				sbg.enterState(1);
 			}
 		}
+		
+		//Launches song in youtube.
+		if ((mouseXPos > 45 && mouseXPos < 412) && (mouseYPos > 392 && mouseYPos < 432)) {
+			if (input.isMouseButtonDown(0)) {
+				try {
+					URI wibw = new URI("https://www.youtube.com/watch?annotation_id=annotation_2000278929&feature=iv&src_vid=zrQyUvxMuKY&v=H1iGOgs4fLE");
+					try {
+						java.awt.Desktop.getDesktop().browse(wibw);
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+				} catch(URISyntaxException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+
 	}
 	
 	public int getID() {
