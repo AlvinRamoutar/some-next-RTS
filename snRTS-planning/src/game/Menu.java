6ebdde1a6fe8f2cd4eaed7b89fb769e665a20496
Alvin <alvinramoutar@gmail.com>
2016-09-18 00:21:43 -0400

    Tried playing around with images and text.

--- snRTS-planning/src/game/Menu.java
+++ snRTS-planning/src/game/Menu.java
@@ -15,6 +15,14 @@ public class Menu extends BasicGameState {
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 
+		Image background = new Image("res/img.jpg");
+		g.drawImage(background, 0, 0);
+		
+		g.drawString("That place between asleep and awake,", 50, 50);
+		g.drawString("where you can still remember dreams...", 70, 70);
+		g.drawString("...that's where I'll always love you,", 200, 420);
+		g.drawString("and thats where I'll be waiting.", 220, 440);
+
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
