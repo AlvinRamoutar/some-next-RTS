37466f85a76de448acedcc34f1e8e6f9604787a9
Alvin <alvinramoutar@gmail.com>
2016-09-22 10:24:57 -0400

    Finished menu interaction and option flags.

--- snRTS-planning/src/game/menu/MainMenu.java
+++ snRTS-planning/src/game/menu/MainMenu.java
@@ -1,17 +1,22 @@
 package game.menu;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.*;
 
 import game.GV;
 
 public class MainMenu extends BasicGameState {
 		
-	Image bg0;
-	Image bg1;
-	int bgPulseCount = 0;
-	int deltaCount = 0;
-	boolean doPulse = false;
+	private Image bg0;
+	private Image bg1;
+	private int bgPulseCount = 0;
+	private boolean doPulse = false;
+	
+	private Image optionsPlaceholder;
+	private MouseOverArea[] menuOptions = new MouseOverArea[5];
+	private float[] menuOptionOpacity = new float[5];
+	private boolean[] menuOptionOpacityStatus = new boolean[5];
 	
 	//Constructor
 	public MainMenu(int state) {
@@ -27,7 +32,17 @@ public class MainMenu extends BasicGameState {
 		//AngelCodeFont Initializing
 		GV.FONTBOLD_COMFORTAA = new AngelCodeFont("res/fonts/Comfortaa/Comfortaa-Bold.fnt","res/fonts/Comfortaa/Comfortaa-Bold.png");
 		GV.FONTLIGHT_COMFORTAA = new AngelCodeFont("res/fonts/Comfortaa/Comfortaa-Light.fnt","res/fonts/Comfortaa/Comfortaa-Light.png");
-
+		
+		//Defining mouseOver area for options.
+		optionsPlaceholder = new Image("res/images/menu/optionsPlaceholder.jpg");
+		for(int a = 0; a < menuOptions.length; a++) {
+			menuOptions[a] = new MouseOverArea(gc, optionsPlaceholder, 454, 318 + (a*30), 188, 20);
+			menuOptions[a].setNormalColor(new Color(255,255,255,0f));
+			menuOptions[a].setMouseOverColor(new Color(255,255,255,0f));
+			menuOptions[a].setMouseDownColor(new Color(255,255,255,0f));
+			menuOptionOpacity[a] = 0.5f;
+		}
+		
 	}
 	
 	//Draw objects to state/window.
@@ -37,12 +52,28 @@ public class MainMenu extends BasicGameState {
 		
 		GV.FONTBOLD_COMFORTAA.drawString(30, 85, "SOME NEXT RTS", new org.newdawn.slick.Color(255, 255, 255, 0.60f));
 		GV.FONTLIGHT_COMFORTAA.drawString(5, 5, GV.VERSION, new org.newdawn.slick.Color(255, 255, 255, 0.5f));
-		GV.FONTLIGHT_COMFORTAA.drawString(454, 318, "SINGLE PLAYER [WIP]", new org.newdawn.slick.Color(255, 255, 255, 0.5f));
-		GV.FONTLIGHT_COMFORTAA.drawString(518, 348, "MULTIPLAYER", new org.newdawn.slick.Color(255, 255, 255, 0.5f));
-		GV.FONTLIGHT_COMFORTAA.drawString(556, 378, "OPTIONS", new org.newdawn.slick.Color(255, 255, 255, 0.5f));
-		GV.FONTLIGHT_COMFORTAA.drawString(590, 408, "HELP", new org.newdawn.slick.Color(255, 255, 255, 0.5f));
-		GV.FONTLIGHT_COMFORTAA.drawString(592, 438, "QUIT", new org.newdawn.slick.Color(255, 255, 255, 0.5f));
+		GV.FONTLIGHT_COMFORTAA.drawString(454, 318, "SINGLE PLAYER [WIP]", new org.newdawn.slick.Color(255, 255, 255, menuOptionOpacity[0]));
+		GV.FONTLIGHT_COMFORTAA.drawString(518, 348, "MULTIPLAYER", new org.newdawn.slick.Color(255, 255, 255, menuOptionOpacity[1]));
+		GV.FONTLIGHT_COMFORTAA.drawString(556, 378, "OPTIONS", new org.newdawn.slick.Color(255, 255, 255, menuOptionOpacity[2]));
+		GV.FONTLIGHT_COMFORTAA.drawString(590, 408, "HELP", new org.newdawn.slick.Color(255, 255, 255, menuOptionOpacity[3]));
+		GV.FONTLIGHT_COMFORTAA.drawString(592, 438, "QUIT", new org.newdawn.slick.Color(255, 255, 255, menuOptionOpacity[4]));
 
+		//Preparing areas for menu options.
+		//Also adjusts opacity as per mouseover.
+		for (int a=0; a < menuOptions.length; a++) {
+			menuOptions[a].render(gc, g);
+			
+			if(menuOptionOpacityStatus[a] == true) {
+				if(menuOptionOpacity[a] < 1) {
+					menuOptionOpacity[a] += 0.001f;
+				}
+			} else {
+				if(menuOptionOpacity[a] > 0.5) {
+					menuOptionOpacity[a] -= 0.001f;
+				}
+			}
+		}	
+	
 		
 		// Adjusts the opacity of background images to create a pulsating effect.
 		if (doPulse == false) {
@@ -62,6 +93,31 @@ public class MainMenu extends BasicGameState {
 		if (bgPulseCount > 500) {
 			doPulse = !doPulse;
 			bgPulseCount = 0;
+		}
+		
+		//Handles changing of option opacity, and actions upon clicking said option.
+		for(int a=0; a < menuOptions.length; a++) {
+			if(menuOptions[a].isMouseOver()) {
+				menuOptionOpacityStatus[a] = true;
+			} else {
+				menuOptionOpacityStatus[a] = false;
+			}
+			
+			if(menuOptions[a].isMouseOver() && gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+				switch(a) {
+					case 0:
+						break;
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						gc.exit();
+						break;
+				}
+			}
 		}
 	}
 	
