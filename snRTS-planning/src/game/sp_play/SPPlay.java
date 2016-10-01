66238c1a7e5374f9e9d7208c7a03b08dfd79e189
Alvin <alvinramoutar@gmail.com>
2016-10-01 18:11:46 -0400

    Tweaking prompt transition.

--- snRTS-planning/src/game/sp_play/SPPlay.java
+++ snRTS-planning/src/game/sp_play/SPPlay.java
@@ -1,6 +1,7 @@
 package game.sp_play;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.*;
 
 import game.GV;
@@ -12,9 +13,15 @@ public class SPPlay extends BasicGameState {
 	private Image x, checkmark;
 	private int promptTabXPos = -225;
 	private int promptTransitionCount = 0;
-	private boolean isPromptEntering = false;
-	private boolean isPromptLeaving = false;
+	private int promptElementXPos = -225;
 	
+	private boolean isPromptNeeded = true;
+	private boolean isPromptActive = false;
+	private boolean isPromptTransitioning = false;
+	private boolean isPromptReadyForInteraction = false;
+	
+	
+	private MouseOverArea[] promptOptions = new MouseOverArea[2];
 	
 	//Constructor
 	public SPPlay(int state) {
@@ -29,6 +36,7 @@ public class SPPlay extends BasicGameState {
 		checkmark = new Image("res/images/play/prompt/checkmark.png");
 		
 		GV.FONTLIGHT_EXO = new AngelCodeFont("res/fonts/Exo/Exo-Light.fnt", "res/fonts/Exo/Exo-Light.png");
+		
 	}
 	
 	//Draw objects to state/window.
@@ -37,17 +45,35 @@ public class SPPlay extends BasicGameState {
 		
 		// Simple yes/no acknowledgement window.
 		g.drawImage(promptTab, promptTabXPos,276);
-		if(isPromptEntering == true && isPromptLeaving == false) {
-			g.drawImage(checkmark, 192, 280);
-			g.drawImage(x, 192, 310);
-			GV.FONTLIGHT_EXO.drawString(5, 280, "This is a test prompt.");
-			GV.FONTLIGHT_EXO.drawString(5, 296, "Do you agree?");
+		
+		if(isPromptNeeded && isPromptTransitioning == false && isPromptActive) {
+
+				g.drawImage(checkmark, 192 + promptElementXPos, 280);
+				g.drawImage(x, 192 + promptElementXPos, 310);
+				
+				promptOptions[0] = new MouseOverArea(gc, checkmark, 192 + promptElementXPos, 280, 24, 24);
+				
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 280, "This is a test prompt.");
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 296, "Do you agree?");
+				
+				//isPromptReadyForInteraction = true;
+				
 		}
+	
+		//promptOptionOpacity[a] = 0.5f;
 	}
 	
 	//Interact with drawn objects through updates.
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		promptTransition(delta);
+		
+		//for(int a=0; a < promptOptions.length; a++) {
+		if(isPromptReadyForInteraction) {
+			if(promptOptions[0].isMouseOver() && gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+				isPromptNeeded = false;
+				System.out.println("Being dlicked");
+			}
+		}
 	}
 	
 	//ID of the current state. Change '?' accordingly.
@@ -57,20 +83,34 @@ public class SPPlay extends BasicGameState {
 	
 	public void promptTransition(int delta) {
 		promptTransitionCount += delta;
-		if(promptTransitionCount > 2 && isPromptEntering == false) {
+		if(promptTransitionCount > 2 && isPromptActive == false && isPromptNeeded) {
 			promptTabXPos += 1;
 			promptTransitionCount = 0;
+			isPromptTransitioning = true;
 			if(promptTabXPos == 0) {
-				isPromptEntering = true;
+				isPromptActive = true;
+				isPromptTransitioning = false;
 			}
 		}
-		if(promptTransitionCount > 2 && isPromptLeaving == true) {
+		if(promptTransitionCount > 2 && isPromptActive == true && isPromptNeeded == false) {
 			promptTabXPos -= 1;
 			promptTransitionCount = 0;
+			isPromptTransitioning = true;
 			if(promptTabXPos == -225) {
-				isPromptLeaving = false;
+				isPromptReadyForInteraction = false;
+				isPromptActive = false;
+				isPromptTransitioning = false;
+				System.out.println("cant click be botch");
 			}
 		}
+		if(promptTransitionCount > 2 && isPromptTransitioning == false && isPromptActive) {
+			promptElementXPos += 1;
+			promptTransitionCount = 0;
+			if(promptElementXPos == 0) {
+				isPromptReadyForInteraction = true;
+			}
+		}
+		
 	}
 	
 }
