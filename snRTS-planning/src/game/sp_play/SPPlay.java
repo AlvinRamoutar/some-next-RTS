ce6470bb570fad1511b1389a051760c8f69dc8d6
Alvin <alvinramoutar@gmail.com>
2016-10-01 19:22:33 -0400

    Finished simple prompt. Still requires a method of handling actions.

--- snRTS-planning/src/game/sp_play/SPPlay.java
+++ snRTS-planning/src/game/sp_play/SPPlay.java
@@ -17,7 +17,6 @@ public class SPPlay extends BasicGameState {
 	
 	private boolean isPromptNeeded = true;
 	private boolean isPromptActive = false;
-	private boolean isPromptTransitioning = false;
 	private boolean isPromptReadyForInteraction = false;
 	
 	
@@ -43,24 +42,9 @@ public class SPPlay extends BasicGameState {
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.drawImage(guiBacking,0,0);
 		
-		// Simple yes/no acknowledgement window.
-		g.drawImage(promptTab, promptTabXPos,276);
-		
-		if(isPromptNeeded && isPromptTransitioning == false && isPromptActive) {
+		//Draws the, well, notification prompt.
+		drawPrompt(g, gc, "line1", "line2", "line3", 1);
 
-				g.drawImage(checkmark, 192 + promptElementXPos, 280);
-				g.drawImage(x, 192 + promptElementXPos, 310);
-				
-				promptOptions[0] = new MouseOverArea(gc, checkmark, 192 + promptElementXPos, 280, 24, 24);
-				
-				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 280, "This is a test prompt.");
-				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 296, "Do you agree?");
-				
-				//isPromptReadyForInteraction = true;
-				
-		}
-	
-		//promptOptionOpacity[a] = 0.5f;
 	}
 	
 	//Interact with drawn objects through updates.
@@ -82,33 +66,66 @@ public class SPPlay extends BasicGameState {
 	
 	public void promptTransition(int delta) {
 		promptTransitionCount += delta;
+		
+		//Transition tab into existence.
 		if(promptTransitionCount > 2 && isPromptActive == false && isPromptNeeded) {
-			promptTabXPos += 1;
-			promptTransitionCount = 0;
-			isPromptTransitioning = true;
-			if(promptTabXPos == 0) {
-				isPromptActive = true;
-				isPromptTransitioning = false;
-			}
-		}
-		if(promptTransitionCount > 2 && isPromptActive == true && isPromptNeeded == false) {
-			promptTabXPos -= 1;
-			promptTransitionCount = 0;
-			isPromptTransitioning = true;
-			if(promptTabXPos == -225) {
-				isPromptReadyForInteraction = false;
-				isPromptActive = false;
-				isPromptTransitioning = false;
-			}
-		}
-		if(promptTransitionCount > 2 && isPromptTransitioning == false && isPromptActive) {
-			promptElementXPos += 1;
-			promptTransitionCount = 0;
+
 			if(promptElementXPos == 0 || promptElementXPos > 0) {
 				isPromptReadyForInteraction = true;
+			} else {
+				promptElementXPos += 1;
+				promptTransitionCount = 0;
+			}
+			
+			if(promptTabXPos == 0) {
+				isPromptActive = true;
+			} else {
+				promptTabXPos += 1;
+				promptTransitionCount = 0;
 			}
 		}
 		
+		//Transition tab out of existence.
+		if(promptTransitionCount > 2 && isPromptActive == true && isPromptNeeded == false) {
+			if(promptTabXPos == -225) {
+				isPromptReadyForInteraction = false;
+				isPromptActive = false;
+			} else {
+				promptTabXPos -= 1;
+				promptTransitionCount = 0;
+			}
+			
+			if(promptElementXPos == -225 || promptElementXPos < -225) {
+				isPromptReadyForInteraction = false;
+			} else {
+				promptElementXPos -= 1;
+				promptTransitionCount = 0;
+			}
+		}
+	}
+	
+	public void drawPrompt(Graphics g, GameContainer gc, String pTextL1, String pTextL2, String pTextL3, int promptType) {
+		switch(promptType) {
+			case 1:
+				g.drawImage(promptTab, promptTabXPos,276);
+				g.drawImage(checkmark, 192 + promptElementXPos, 280);
+				promptOptions[0] = new MouseOverArea(gc, checkmark, 192 + promptElementXPos, 280, 24, 24);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 280, pTextL1);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 296, pTextL2);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 312, pTextL3);
+				break;
+			case 2:
+				g.drawImage(promptTab, promptTabXPos,276);
+				g.drawImage(checkmark, 192 + promptElementXPos, 280);
+				g.drawImage(x, 192 + promptElementXPos, 310);
+				promptOptions[0] = new MouseOverArea(gc, checkmark, 192 + promptElementXPos, 280, 24, 24);
+				promptOptions[1] = new MouseOverArea(gc, checkmark, 192 + promptElementXPos, 310, 24, 24);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 280, pTextL1);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 296, pTextL2);
+				GV.FONTLIGHT_EXO.drawString(5 + promptElementXPos, 312, pTextL3);
+				break;
+		}
+
 	}
 	
 }
